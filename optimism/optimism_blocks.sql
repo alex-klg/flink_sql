e80650efbe9937565f0dc72a46d4db38dc5a22ad
alexKLG <hxchen@riches.ai>
2022-12-09 10:14:08 +0800

    init

---
+++ optimism/optimism_blocks.sql
@@ -0,0 +1,75 @@
+-- Define available catalogs
+CREATE CATALOG iceberg WITH (
+  'type'='iceberg',
+  'catalog-type'='hive',
+  'uri'='thrift://10.106.192.22:9083',
+  'clients'='5',
+  'property-version'='1',
+  'warehouse'='gs://footbase-prod/hive-warehouse'
+);
+
+-- 在 trino 执行 show create table 就可以拿到建表语句了
+CREATE TABLE kf_optimism_blocks
+(
+   base_fee_per_gas decimal(38, 0),
+   transaction_count bigint,
+   `timestamp` bigint,
+   gas_used decimal(38, 0),
+   gas_limit decimal(38, 0),
+   extra_data STRING,
+   size decimal(38, 0),
+   total_difficulty decimal(38, 0),
+   difficulty decimal(38, 0),
+   miner varchar,
+   receipts_root varchar,
+   state_root varchar,
+   transactions_root varchar,
+   logs_bloom STRING,
+   sha3_uncles varchar,
+   nonce varchar,
+   parent_hash varchar,
+   hash STRING,
+   number bigint
+) WITH (
+    'connector' = 'kafka',
+    'topic' = 'chain_data_optimism_blocks',
+    'properties.bootstrap.servers' = '10.202.0.114:9092',
+    'properties.group.id' = 'flink_read_group',
+    'format' = 'json',
+    'scan.startup.mode' = 'latest-offset',
+    'json.fail-on-missing-field' = 'false',
+    'json.ignore-parse-errors' = 'true'
+);
+
+-- iceberg table 在 trino 那边建好
+
+-- set checkpointing 写入 iceberg 的时间间隔
+SET 'execution.checkpointing.interval' = '4min';
+SET 'pipeline.name' = 'optimism_blocks_sync';
+
+
+-- submit job
+insert into iceberg.prod_bronze.optimism_blocks
+select
+    base_fee_per_gas,
+    transaction_count,
+    to_timestamp(from_unixtime(`timestamp`, 'yyyy-MM-dd HH:mm:ss')) as `timestamp`,
+    gas_used,
+    gas_limit,
+    extra_data,
+    size,
+    total_difficulty,
+    difficulty,
+    miner,
+    receipts_root,
+    state_root,
+    transactions_root,
+    logs_bloom,
+    sha3_uncles,
+    nonce,
+    parent_hash,
+    hash,
+    number
+from kf_optimism_blocks;
+
+
---
+++ optimism/optimism_transactions.sql
@@ -0,0 +1,70 @@
+-- Define available catalogs
+CREATE CATALOG iceberg WITH (
+  'type'='iceberg',
+  'catalog-type'='hive',
+  'uri'='thrift://10.106.192.22:9083',
+  'clients'='5',
+  'property-version'='1',
+  'warehouse'='gs://footbase-prod/hive-warehouse'
+);
+
+-- Define table
+-- 在 trino 执行 show ca
+CREATE TABLE kf_optimism_transactions
+(
+   hash STRING,
+   nonce bigint,
+   transaction_index bigint,
+   from_address varchar,
+   to_address STRING,
+   `value` decimal(38, 0),
+   gas bigint,
+   gas_price bigint,
+   input STRING,
+   receipt_cumulative_gas_used bigint,
+   receipt_gas_used bigint,
+   receipt_contract_address varchar,
+   receipt_root varchar,
+   receipt_status bigint,
+   -- timestamp(3),
+   `block_timestamp` bigint,
+   block_number bigint,
+   block_hash varchar,
+   max_fee_per_gas bigint,
+   max_priority_fee_per_gas bigint,
+   transaction_type bigint,
+   receipt_effective_gas_price bigint
+) WITH (
+    'connector' = 'kafka',
+    'topic' = 'chain_data_optimism_transactions',
+    'properties.bootstrap.servers' = '10.202.0.114:9092',
+    'properties.group.id' = 'flink_read_group',
+    'format' = 'json',
+    'scan.startup.mode' = 'latest-offset',
+    'json.fail-on-missing-field' = 'false',
+    'json.ignore-parse-errors' = 'true'
+);
+
+SET 'execution.checkpointing.interval' = '4min';
+-- SET 'execution.checkpointing.interval' = '10s';
+SET 'pipeline.name' = 'optimism_transactions_sync';
+-- SET 'table.dml-sync' = 'true';
+
+-- select CAST('' AS bigint)
+-- select to_timestamp(1666781543) as ts
+-- select to_timestamp(from_unixtime(1666781543, 'yyyy-MM-dd HH:mm:ss'))
+-- select * from kf_optimism_transactions;
+insert into iceberg.beta_bronze.optimism_transactions
+select
+    hash, nonce, transaction_index,
+    from_address, to_address, `value`,
+    gas, gas_price, `input`,
+    receipt_cumulative_gas_used,
+    receipt_gas_used, receipt_contract_address,
+    receipt_root, receipt_status,
+    to_timestamp(from_unixtime(block_timestamp, 'yyyy-MM-dd HH:mm:ss')) as block_timestamp,
+    block_number, block_hash,
+    max_fee_per_gas,
+    max_priority_fee_per_gas,
+    transaction_type, receipt_effective_gas_price
+from kf_optimism_transactions;
